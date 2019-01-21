75e9fa5ca2ec3b7936a57a131ef916772746ff64
Florin <furtuna.florin90@gmail.com>
2019-01-21 13:59:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,10 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 function removeElementFromBeginningOfArray(chocolateBars){
   return chocolateBars.slice(1);
 }
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
 
 
 
