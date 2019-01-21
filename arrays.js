5fc64bc535853677278326a3b396f7954f575124
Florin <furtuna.florin90@gmail.com>
2019-01-21 13:56:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,7 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+ 
